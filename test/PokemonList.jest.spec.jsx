2d76635347e0062b05ec4c423808c636aa98d1bc
Ricardo Miralles <ricmiber@gmail.com>
2022-05-11 19:37:15 +0200

    Build and test app in deployment

--- test/PokemonList.jest.spec.jsx
+++ test/PokemonList.jest.spec.jsx
@@ -22,7 +22,7 @@ describe('<PokemonList />', () => {
         <PokemonList pokemonList={pokemonList} />
       </BrowserRouter>
     )
-    expect(screen.getByText('bulbasaur')).toBeVisible()
+    expect(screen.getByText('charmander')).toBeVisible()
     expect(screen.getByText('eevee')).toBeVisible()
   })
 })
